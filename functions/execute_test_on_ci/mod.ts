6bb02a3795bb0c3f9242b420a53efded21ab0bd8
dhythm <yuta.okada.20@gmail.com>
2023-01-08 03:22:29 +0900

    update execute_test_on_ci/mod.ts

--- functions/execute_test_on_ci/mod.ts
+++ functions/execute_test_on_ci/mod.ts
@@ -1,7 +1,9 @@
 import { ExecuteTestOnCiFunction } from "./definition.ts";
 import { SlackAPI } from "deno-slack-api/mod.ts";
 import { SlackFunction } from "deno-slack-sdk/mod.ts";
+import { Buffer } from "https://deno.land/std/io/buffer.ts";
 import executionTestHeaderBlocks from "./blocks.ts";
+import { CIRCLE_CI_TOKEN, GITHUB_ACCOUNT_NAME, GITHUB_REPOSITORY_NAME } from "./secrets.ts";
 
 // Custom function that sends a message to the user's manager asking
 // for approval for the time off request. The message includes some Block Kit with two
@@ -15,6 +17,24 @@ export default SlackFunction(
     // Create a block of Block Kit elements composed of several header blocks
     // plus the interactive approve/deny buttons at the end
     const blocks = executionTestHeaderBlocks(inputs);
+
+    const url = `https://circleci.com/api/v2/project/gh/${GITHUB_ACCOUNT_NAME}/${GITHUB_REPOSITORY_NAME}/pipeline`;
+    const data = {
+      branch: `${inputs.branch}`,
+      parameters: { manual_execution: true },
+    }
+    const encodedCiToken = new Buffer(new TextEncoder().encode(CIRCLE_CI_TOKEN + ":")).toString('base64');
+
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        Authorization: `Basic ${encodedCiToken}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data)
+    })
+    console.log({ ...blocks })
+    console.log({ ...res })
 
     // Send the message to the manager
     const msgResponse = await client.chat.postMessage({
---
+++ functions/execute_test_on_ci/secrets.example.ts
@@ -0,0 +1,3 @@
+export const GITHUB_ACCOUNT_NAME = 'xxx'
+export const GITHUB_REPOSITORY_NAME = 'xxx'
+export const CIRCLE_CI_TOKEN = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
